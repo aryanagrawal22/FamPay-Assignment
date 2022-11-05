6ef6cf75c6df39f8bb9acc50f253b1c4c84fb690
aryanagrawal22 <aryanagrawal20023@gmail.com>
2022-11-05 14:20:48 +0530

    ApiKey: Multiple keys feature added

--- src/jobs/video.job.js
+++ src/jobs/video.job.js
@@ -5,31 +5,54 @@ const YoutubeModel = require("../models/youtube.model")
 const logger = require('../utils/log.util');
 const errorlogger = require('../utils/error.log.util');
 
+// Get keys from the env file and split it to save it in array
+const keys = process.env.YOUTUBE_API_KEY.split('|')
+  let youtube = google.youtube({
+    version: "v3",
+    auth: keys[0],
+  });
+
 async function getVideo() {
   try {
 
     // Set a cron job for hitting request in every 1 minute
     cron.schedule("* * * * *", async () => {
 
-      const youtube = google.youtube({
-        version: "v3",
-        auth: process.env.YOUTUBE_API_KEY,
-      });
-
       // DateTime of 1 minute earlier time and search for videos uploaded after that
       const publishedAfter = dayjs().subtract(1, "minute").toISOString();
 
       // Topic to search (Example: News)
-      const q = process.env.YOUTUBE_SEARCH_QUERY
+      const query = process.env.YOUTUBE_SEARCH_QUERY
 
       const fetched = await youtube.search.list({
         part: ["snippet"],
         maxResults: 50,
         order: "date",
-        q,
+        query,
         relevanceLanguage: "en",
         publishedAfter,
+
+      }).catch(err => {
+        // If API key expired then switch to new key by popping the 0th key and using the new 0th key
+        if(err.errors[0].message=="API key expired. Please renew the API key.") {
+          const newApiKey = keys.shift();
+
+          //If all keys finished then send warning error
+          if(keys.length==0) errorlogger.info(`All API keys used.`);
+
+          //Set new key
+          youtube = google.youtube({
+            version: "v3",
+            auth: newApiKey,
+          });
+          logger.info(`Current API key expired. New API key- ${newApiKey}`);
+          console.log(`Current API key expired. New API key- ${newApiKey}`);
+        }else{
+          console.log("ERROR: /api/videos (View error.log)")
+          errorlogger.info("No response from API, check parameters");
+        }
       });
+
 
       // Relevent data into array format of fetched data
       const videos = fetched.data.items.map((item) => ({
