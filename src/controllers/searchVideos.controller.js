b5f8cd8efca7ad349e78211abb7ca3e6f1ff9087
aryanagrawal22 <aryanagrawal20023@gmail.com>
2022-11-05 14:55:37 +0530

    SearchVideo: Pagination fix

--- src/controllers/searchVideos.controller.js
+++ src/controllers/searchVideos.controller.js
@@ -12,7 +12,7 @@ async function searchVideos(req, res) {
     const regexQuery = new RegExp(query.replace("-", "|"));
 
     // Find all matching videos with the regex comparison and option: i (match both lower and upper case)
-    const videos = await YoutubeModel.find({
+    let videos = await YoutubeModel.find({
       $or: [
         { title: { $regex: regexQuery, $options: "i" } },
         { description: { $regex: regexQuery, $options: "i" } },
@@ -25,13 +25,16 @@ async function searchVideos(req, res) {
     // Total pages that can be formed
     const totalPages = Math.ceil(totalItems / 10);
 
-    // // If page greater than total then set it to last or if does not exist then set default to page 1
+    // If page greater than total then set it to last or if does not exist then set default to page 1
     const page = req.query.page > totalPages ? totalPages : req.query.page || 1;
 
     // Calculate haivng next and having previous pages or not
     const hasPrev = page > 1;
     const hasNext = page < totalPages;
     const pageItems = page!=totalPages?10:totalItems - (totalPages-1)*10;
+
+    // Send videos of that certain page
+    videos = videos.slice((page-1)*10, (page-1)*10+pageItems);
 
     res.status(200).json({
       videos,
